4da395573c225e8910e4c1c8800b0392f5d15fa7
yenn_l0 <82251475+yeeun03030@users.noreply.github.com>
2024-06-30 22:28:18 +0900

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "we_can_be_rich"
+include(":app")
